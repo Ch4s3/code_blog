3f1b7d9a2af4fe006b816ba8718e4881667b50d1
Chase <chase.gilliam@gmail.com>
2015-11-21 18:23:02 -0500

    added github flavored markdown option

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,9 +1,10 @@
 defmodule CodeBlog.PostView do
   use CodeBlog.Web, :view
+  alias Earmark.Options
 
   def markdown(body) do
     body
-    |> Earmark.to_html
+    |> Earmark.to_html(body,  %Options{gfm: true})
     |> raw
   end
 end
