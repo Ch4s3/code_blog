ab41840d224ddc4eb61138b5e4e17b4d8a9e8ba2
Chase <chase.gilliam@gmail.com>
2015-11-22 23:52:59 -0500

    update role form and deps

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule CodeBlog.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.2.2"},
+     {:comeonin, "~> 1.6.0"},
      {:earmark, "~> 0.1.19"}]
   end
 
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,9 @@
 defmodule CodeBlog.UserView do
   use CodeBlog.Web, :view
+
+  def roles_for_select(roles) do
+    roles
+    |> Enum.map(&["#{&1.name}": &1.id])
+    |> List.flatten
+  end
 end
